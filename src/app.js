3d9b50b2386206417582c0e136ca7ecf2a8218d4
Suraj Kumar <68435934+Surajsinhar77@users.noreply.github.com>
2024-12-05 22:41:33 +0530

    change in app.js for morgon module

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 
 const app = express()
+import morgon from "morgon"
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -13,6 +14,7 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("publiic"))
 app.use(cookieParser())
+app.use(morgon('dev'))
 
 // routes import 
 import userRouter from "./routes/user.routes.js"
